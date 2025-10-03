26724008ede90a53f4c54caa0d4bb94844cdcf28
AtharvSaini15 <sainiatharv92@gmail.com>
2025-10-03 22:56:43 +0530

    Add files via upload

---
+++ 44 day coding/44 day q1.c
@@ -0,0 +1,38 @@
+/*Q87: Count spaces, digits, and special characters in a string.*/
+
+/*
+Sample Test Cases:
+Input 1:
+a b1&2
+Output 1:
+Spaces=1, Digits=2, Special=1
+
+*/
+#include <stdio.h>
+#include <ctype.h>   
+int main()
+ {
+    char str[200];
+    int i, spaces = 0, digits = 0, special = 0;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    for (i = 0; str[i] != '\0'; i++) 
+	{
+        if (isspace(str[i]))
+		 {
+            spaces++;
+        }
+        else if (isdigit(str[i])) 
+		{
+            digits++;
+        }
+        else if (!isalpha(str[i])) 
+		{  
+            special++;
+        }
+    }
+    printf("Spaces: %d\n", spaces);
+    printf("Digits: %d\n", digits);
+    printf("Special Characters: %d\n", special);
+    return 0;
+}
